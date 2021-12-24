c8516cc34cd9818f435fd5c8a166b29a36a5f4f8
Marcos Wellington <marcoswoc@gmail.com>
2021-12-24 10:12:45 -0300

    Adicionando imagens

--- Pessoa.cs
+++ Pessoa.cs
@@ -1,7 +1,12 @@
+using System.IO;
+
 namespace exemplo_vscode_dotnet
 {
     public class Pessoa
     {
         public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Pessoa() { }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿try
+﻿using exemplo_vscode_dotnet;
+
+var pessoa = new Pessoa();
+
+pessoa.Nome = "João";
+pessoa.Idade = 29;
+
+var pessoa2 = new Pessoa();
+
+pessoa2.Nome = "Maria";
+pessoa2.Idade = 50;
